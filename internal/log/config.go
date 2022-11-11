91c9bb9ed492897129257ec494a28ae964bb8f24
yurii-vyrovyi <y.vyrovyi@gmail.com>
2022-11-11 17:40:45 +0100

    moving files

--- internal/log/config.go
+++
@@ -1,7 +0,0 @@
-package log
-
-type Config struct {
-	Level   string `json:"level" yaml:"level" split_words:"true"`
-	Pretty  bool   `json:"pretty" yaml:"pretty" split_words:"true"`
-	NonJson bool   `json:"nonJson" yaml:"non-json" split_words:"true"`
-}
